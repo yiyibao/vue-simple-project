476eacfdc33bea877752d239b1803c3f5215c3dd
baolilei <baolilei@tal100.com>
2018-07-16 18:00:50 +0800

    优化提示

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -50,11 +50,11 @@ module.exports = merge(baseWebpackConfig, {
         if (severity !== 'error') {
           return;
         }
-        const error = errors[0];
+        const filename = error.file && error.file.split('!').pop()
         notifier.notify({
           title: "Webpack error",
           message: severity + ': ' + error.name,
-          subtitle: error.file || '',
+          subtitle: filename || '',
           icon: ICON
         });
       }
