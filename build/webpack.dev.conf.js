4b12c1d53f16c45a855e6704af60cc2b561f8fcd
baolilei <baolilei@tal100.com>
2018-07-16 17:39:57 +0800

    优化提示

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,6 +1,11 @@
 const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf')
 const url =require('url')
+const path = require('path')
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+const notifier = require('node-notifier')
+var ICON = path.join(__dirname, 'logo.png')
+
 module.exports = merge(baseWebpackConfig, {
 	module: {
 		rules: [{
@@ -16,7 +21,8 @@ module.exports = merge(baseWebpackConfig, {
 	},
 	devServer: {
 		host: '127.0.0.1',
-		port: 8808,
+    port: 8808,
+    quiet:true,
 		proxy: {
 			'/api': {
 				target: 'http://127.0.0.1:3000',
@@ -30,6 +36,26 @@ module.exports = merge(baseWebpackConfig, {
 		historyApiFallback: {
 			index: url.parse('/assets/').pathname
 		}
-	},
+  },
+  plugins:[
+    new FriendlyErrorsPlugin({
+      compilationSuccessInfo: {
+        messages: [`Your application is running here: http://127.0.0.1:8808`],
+        notes: []
+      },
+      onErrors: (severity, errors) => {
+        if (severity !== 'error') {
+          return;
+        }
+        const error = errors[0];
+        notifier.notify({
+          title: "Webpack error",
+          message: severity + ': ' + error.name,
+          subtitle: error.file || '',
+          icon: ICON
+        });
+      }
+    })
+  ],
 	devtool:'#eval-source-map'
 })
